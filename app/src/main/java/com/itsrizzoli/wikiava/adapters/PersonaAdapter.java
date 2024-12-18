b2606fe201e6e5ab64c2e298dfc731156d593c4c
fralnz <francesco@fragiolanza.eu>
2024-12-18 14:31:06 +0100

    sistemato la chiamata al singleton

--- app/src/main/java/com/itsrizzoli/wikiava/adapters/PersonaAdapter.java
+++ app/src/main/java/com/itsrizzoli/wikiava/adapters/PersonaAdapter.java
@@ -2,6 +2,7 @@ package com.itsrizzoli.wikiava.adapters;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +12,11 @@ import android.widget.TextView;
 import com.itsrizzoli.wikiava.R;
 import com.itsrizzoli.wikiava.models.DataList;
 
+import java.util.Objects;
+
 public class PersonaAdapter extends CursorAdapter {
     DataList dataList = DataList.getInstance();
+
     public PersonaAdapter(Context context, Cursor c) {
         super(context, c, 0);
     }
@@ -25,14 +29,32 @@ public class PersonaAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        String nome = cursor.getString(cursor.getColumnIndex("nome"));
-        String id = cursor.getString(cursor.getColumnIndex("_id"));
-        String bodyCount = dataList.getBodyCountMap().get(Integer.parseInt(id)).toString();
+        // Debugging logs
+        int nomeIndex = cursor.getColumnIndex("nome");
+        int genereIndex = cursor.getColumnIndex("genere");
 
+        if (nomeIndex >= 0) {
+            Log.d("PersonaAdapter", "nome: " + cursor.getString(nomeIndex));
+        } else {
+            Log.e("PersonaAdapter", "Column 'nome' not found!");
+        }
+
+        // Safely retrieve ID
+        int id = cursor.getInt(cursor.getColumnIndex("_id"));
+
+        // Safely retrieve Body Count
+        String bodyCount = dataList.getBodyCountMap() != null && dataList.getBodyCountMap().containsKey(id)
+                ? dataList.getBodyCountMap().getOrDefault(id, 0).toString()
+                : "0";
+
+        // Safely retrieve Nome
+        String nome = cursor.isNull(nomeIndex) ? "Unknown" : cursor.getString(nomeIndex);
+
+        // Bind data
         TextView nomeTextView = view.findViewById(R.id.nomeTextView);
         TextView bodyCountTextView = view.findViewById(R.id.bodyCountTextView);
 
         nomeTextView.setText(nome);
-        bodyCountTextView.setText("Body count: " + bodyCount);
+        bodyCountTextView.setText(bodyCount);
     }
 }
